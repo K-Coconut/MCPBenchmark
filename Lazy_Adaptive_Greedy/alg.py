9b12aed4d3d70eac9fc5db9375529edffe2eb340
K-Coconut <710949644@qq.com>
2022-09-19 17:31:51 +0800

    readme

--- Lazy_Adaptive_Greedy/alg.py
+++ Lazy_Adaptive_Greedy/alg.py
@@ -20,27 +20,27 @@ def lazy_adaptive_greedy(main_graph, k, n=10):
     isCovered = [False for _ in range(num_nodes)]
     solution_set = []
     set_nodes = main_graph.degree().items()
-    all_nodes_queue = []
+    H = []
     for nodeId, init_gain in set_nodes:
-        heappush(all_nodes_queue, (-init_gain, 0, nodeId))
+        heappush(H, (-init_gain, 0, nodeId))
 
-    queue = []
-    while len(all_nodes_queue) != 0 and len(queue) < k * n:
-        marginal_gain, timestamp, nodeId = heappop(all_nodes_queue)
-        heappush(queue, (marginal_gain, timestamp, nodeId))
-    td = - nlargest(1, all_nodes_queue)[0][0]
+    Q = []
+    while len(H) != 0 and len(Q) < k * n:
+        marginal_gain, timestamp, nodeId = heappop(H)
+        heappush(Q, (marginal_gain, timestamp, nodeId))
+    td = - nlargest(1, H)[0][0]
 
     for itr in range(k):
-        marginal_gain, timestamp, nodeId = heappop(queue)
+        marginal_gain, timestamp, nodeId = heappop(Q)
         while -timestamp != itr:
             marginal_gain = calculate_marginal_gains(main_graph.neighbors(nodeId), isCovered)
             if marginal_gain > td:
-                heappush(queue, (-marginal_gain, -itr, nodeId))
+                heappush(Q, (-marginal_gain, -itr, nodeId))
             else:
-                heappush(all_nodes_queue, (-marginal_gain, -itr, nodeId))
-            if len(queue) == 0:
-                queue = all_nodes_queue
-            marginal_gain, timestamp, nodeId = heappop(queue)
+                heappush(H, (-marginal_gain, -itr, nodeId))
+            if len(Q) == 0:
+                Q = H
+            marginal_gain, timestamp, nodeId = heappop(Q)
         solution_set.append(nodeId)
         select_node(nodeId, main_graph.neighbors(nodeId), isCovered)
     return solution_set
